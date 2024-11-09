01b81ac37c0bac26e862c097ad6a1a42af143e86
dzaki <dzakiahmad005@gmail.com>
2024-11-09 10:36:00 +0700

    First commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 function formValidation(event) {
-    const nama = document.getElementById('Nama').value
-    const email = document.getElementById('Email').value
-    const interest = document.getElementById('interest').value
+    const nama = document.getElementById('Nama').value;
+    const email = document.getElementById('Email').value;
+    const interest = document.getElementById('interest').value;
 
-    if (nama === '' || email === '' || interest === '') {
-        window.alert('Inputan tidak boleh kosong!')
-        event.preventDefault()
+    if (nama == '' || email == '' || interest == '') {
+        window.alert('Inputan tidak boleh kosong!');
+        event.preventDefault();
     } else {
-        window.alert('Terimakasih telah menginput')
+        window.alert('Terimakasih telah menginput');
     }
 }
 //Auto Slide
-let indexslide = 1
-showslide(1)
+let indexslide = 1;
+showslide(1);
 function nextslide(n) {
-    showslide((indexslide += n))
+    showslide((indexslide += n));
 }
 function showslide(n) {
-    let listimage = document.getElementsByClassName('slide')
-    console.log(listimage)
-    if (n > listimage.length) indexslide = 1
-    let i = 0
+    let listimage = document.getElementsByClassName('slide');
+    console.log(listimage);
+    if (n > listimage.length) indexslide = 1;
+    let i = 0;
     while (i < listimage.length) {
-        listimage[i].style.display = 'none'
-        i++
+        listimage[i].style.display = 'none';
+        i++;
     }
-    listimage[indexslide - 1].style.display = 'block'
+    listimage[indexslide - 1].style.display = 'block';
 }
-setInterval(() => nextslide(1), 2000)
+setInterval(() => nextslide(1), 2000);
